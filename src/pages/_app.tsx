55328f1c494f55fccffae42bad8ffeea6a8a0c04
Fili <126945152+filiksyos@users.noreply.github.com>
2025-12-19 12:46:53 +0300

    Initial MVP: YouTube thumbnail guessing game with scoring system

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/global.scss";
+import { Be_Vietnam_Pro } from "next/font/google";
+
+const beVietnamPro = Be_Vietnam_Pro({
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	style: ["normal", "italic"],
+	subsets: ["latin"],
+	display: "swap",
+});
+
+export default function App({ Component, pageProps }: any) {
+	return (
+		<main className={beVietnamPro.className}>
+			<Component {...pageProps} />
+		</main>
+	);
+}
